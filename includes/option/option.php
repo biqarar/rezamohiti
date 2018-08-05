56f27c59c0fe86d3e25676f8e1d00e0d8be21ecb
Reza <rm.biqarar@gmail.com>
2018-08-05 21:14:20 +0430

    fix option

--- includes/option/option.php
+++ includes/option/option.php
@@ -1,22 +1,80 @@
 <?php
-/**
- * save logs in other database
- */
+
+
+self::$config['site']['title']  = null;
+self::$config['site']['desc']   = null;
+self::$config['site']['slogan'] = null;
+
 
 /**
 @ In the name Of Allah
-* The base configurations of the dashsample.
+* The base configurations of the jibres.
 */
 self::$language =
 [
-	'default' => 'fa',
+	'default' => 'en',
 	'list'    => ['fa','en',],
 ];
 /**
  * system default lanuage
  */
-self::$config['site']['title']  = "Dash";
-self::$config['site']['desc']   = "Free PHP Framework & CMS!";
-self::$config['site']['slogan'] = "The simple framework for php programmers ;)";
+
+
+self::$config['redirect']                     = 'a';
+
+self::$config['visitor'] = true;
+
+self::$config['favicon']['version']           = null;
+
+
+/**
+ * call kavenegar template
+ */
+self::$config['enter']['call']                = true;
+self::$config['enter']['call_template_fa'] = 'ermile-fa';
+self::$config['enter']['call_template_en'] = 'ermile-en';
+
+
+/**
+ * first signup url
+ * main redirect url . signup redirect url
+ */
+self::$config['enter']['singup_redirect']     = 'a';
+
+
+/**
+ * cronjob urls and status
+ */
+self::$config['cronjob']['status'] = true;
+
+
+/**
+ * list of units
+ */
+self::$config['units'] =
+[
+	1 =>
+	[
+		'title' => 'toman',
+		'desc'  => "Toman",
+	],
+
+	2 =>
+	[
+		'title' => 'dollar',
+		'desc'  => "$",
+	],
+];
+// the unit id for default
+self::$config['default_unit'] = 1;
+// force change unit to this unit
+self::$config['force_unit']   = 1;
+
+
+self::$config['enter']['verify_telegram'] = false;
+self::$config['enter']['verify_sms']      = true;
+self::$config['enter']['verify_call']     = true;
+self::$config['enter']['verify_sendsms']  = false;
+
 
 ?>
