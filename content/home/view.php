0a332183ebe46954b4981182ed8578f6a12f65e2
Reza <rm.biqarar@gmail.com>
2018-08-05 23:38:17 +0430

    add home page

--- content/home/view.php
+++ content/home/view.php
@@ -1,11 +1,12 @@
 <?php
 namespace content\home;
 
+
 class view
 {
 	public static function config()
 	{
-
+		\dash\data::allPostList(\dash\app\posts::get_post_list(['pagenation' => true]));
 	}
 }
 ?>
