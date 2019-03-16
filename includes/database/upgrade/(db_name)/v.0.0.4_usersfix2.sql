2b43aa07043ff3c45fd207c1960819caefb2230e
Reza <rm.biqarar@gmail.com>
2019-03-16 12:15:22 +0330

    fix user

---
+++ includes/database/upgrade/(db_name)/v.0.0.4_usersfix2.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `users` ADD  `website` varchar(200) CHARACTER SET utf8mb4 DEFAULT NULL;
+ALTER TABLE `users` ADD  `facebook` varchar(200) CHARACTER SET utf8mb4 DEFAULT NULL;
+ALTER TABLE `users` ADD  `twitter` varchar(200) CHARACTER SET utf8mb4 DEFAULT NULL;
+ALTER TABLE `users` ADD  `instagram` varchar(200) CHARACTER SET utf8mb4 DEFAULT NULL;
+ALTER TABLE `users` ADD  `linkedin` varchar(200) CHARACTER SET utf8mb4 DEFAULT NULL;
+ALTER TABLE `users` ADD  `gmail` varchar(200) CHARACTER SET utf8mb4 DEFAULT NULL;
+ALTER TABLE `users` ADD  `sidebar` bit(1) DEFAULT NULL;
+ALTER TABLE `users` ADD  `firstname` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL;
+ALTER TABLE `users` ADD  `lastname` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL;
