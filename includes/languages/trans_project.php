a0d0e3a9bc300d5cfe4c105a80dbcb2b0a86b945
Reza <rm.biqarar@gmail.com>
2019-03-16 11:41:55 +0330

    update trans

--- includes/languages/trans_project.php
+++ includes/languages/trans_project.php
@@ -4,18 +4,52 @@ class twigTransTerms
  private function transtext()
  {
 
-	//--------------------------------------------includes/languages/trans_static.html
-	echo T_("Dash");                                                                  // Line 7 Seperate
+	//-----------------------------------------------------------content_i/layout.html
+	echo T_("Manage");                                                                // Line 6
 
-	//--------------------------------------public_html/static/siftal/js/error_page.js
+	//------------------------------------------------------content_i/food/layout.html
+	echo T_("Description");                                                           // Line 12
+	echo T_("Date");                                                                  // Line 18
+	echo T_("Time");                                                                  // Line 26
+	echo T_("title");                                                                 // Line 58
+	echo T_("Add or select");                                                         // Line 80
+	echo T_("subtitle");                                                              // Line 59
+	echo T_("cat");                                                                   // Line 60
+	echo T_("cat2");                                                                  // Line 61
+	echo T_("size");                                                                  // Line 62
+	echo T_("Avatar");                                                                // Line 93
+	echo T_("Add");                                                                   // Line 101
+	echo T_("Save");                                                                  // Line 105
+
+	//-------------------------------------------------content_i/food/add/display.html
+	echo T_("Other");                                                                 // Line 13
+
+	//------------------------------------------------content_i/food/home/display.html
+	echo T_("Search");                                                                // Line 43
+	echo T_("User");                                                                  // Line 56
+	echo T_("image");                                                                 // Line 57
+	echo T_("desc");                                                                  // Line 63
+	echo T_("datetime");                                                              // Line 64
+	echo T_("Clear filters");                                                         // Line 105
+	echo T_("Result not found!");                                                     // Line 104
+	echo T_("Search with new keywords.");                                             // Line 104
+	echo T_("Hi!");                                                                   // Line 111
+	echo T_("Try to start with add new audio!");                                      // Line 111
+
+	//------------------------------------------------content_i/food/edit/display.html
+
+	//-----------------------------------------------------content_i/home/display.html
+	echo T_("Audio bank");                                                            // Line 20
+
+	//---------------------public_html/static/siftal/fonts/siftal/icons-reference.html
+
+	//--------------------------------------public_html/static/siftal/js/siftal.min.js
 
 	//-----------------------public_html/static/siftal/js/highcharts/highcharts.min.js
 
 	//-----------------------------------public_html/static/siftal/js/highlight.min.js
 
-	//--------------------------------------public_html/static/siftal/js/siftal.min.js
-
-	//---------------------public_html/static/siftal/fonts/siftal/icons-reference.html
+	//--------------------------------------public_html/static/siftal/js/error_page.js
 
 	//---------------------------------------------------content/template/dafault.html
 	echo T_("For share via social networks");                                         // Line 40
@@ -29,17 +63,26 @@ class twigTransTerms
 	echo T_("Send");                                                                  // Line 158
 
 	//-------------------------------------------------------content/home/display.html
-	echo T_("Check and Generate National code");                                      // Line 5
+	echo T_("Check and Generate National code");                                      // Line 6
+	echo T_("Abjad");                                                                 // Line 7
+	echo T_("Food");                                                                  // Line 9
 
 	//-----------------------------------------------content/nationalcode/display.html
 	echo T_("Back");                                                                  // Line 5
 	echo T_("National code");                                                         // Line 13
-	echo T_("Check");                                                                 // Line 15
+	echo T_("Check");                                                                 // Line 31
 	echo T_("This is a valid national code");                                         // Line 20
 	echo T_("This is not a valid national code");                                     // Line 22
 	echo T_("To generate new national code click here");                              // Line 26
 	echo T_("GENERATE NEW");                                                          // Line 27
-	echo T_("check it");                                                              // Line 31
+
+	//------------------------------------------------------content/abjad/display.html
+	echo T_("Text");                                                                  // Line 12
+	echo T_("Your text");                                                             // Line 13
+	echo T_("Calc");                                                                  // Line 15
+
+	//--------------------------------------------includes/languages/trans_static.html
+	echo T_("Dash");                                                                  // Line 7 Seperate
 
  }
 }
