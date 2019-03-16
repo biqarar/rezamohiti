639074fd864430110b8d7717921da64f149e51d0
Reza <rm.biqarar@gmail.com>
2019-03-16 10:52:49 +0330

    fix check login

--- content_i/food/add/controller.php
+++ content_i/food/add/controller.php
@@ -6,7 +6,11 @@ class controller
 
 	public static function routing()
 	{
-
+		if(!\dash\user::login())
+		{
+			\dash\redirect::to(\dash\url::kingdom(). '/enter?referer='. \dash\url::pwd(), 'direct');
+			return;
+		}
 	}
 }
 ?>
