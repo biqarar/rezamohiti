17c2843d6be51c2267c6f29c7782dd1133583c65
Reza <rm.biqarar@gmail.com>
2019-07-22 17:23:51 +0430

    update

--- content/view.php
+++ content/view.php
@@ -10,7 +10,9 @@ class view
 		\dash\data::site_desc(T_("Reza Mohiti"));
 		\dash\data::site_slogan(T_("programer;)"));
 		\dash\data::page_desc(\dash\data::site_desc(). ' | '. \dash\data::site_slogan());
+		// we dont use js
 		\dash\data::include_js(false);
+		\dash\data::include_css(false);
 	}
 }
 ?>
