bdbccf08ab76c95f56121c895386a7fb135fb16d
Reza Mohiti <rm.biqarar@gmail.com>
2020-07-09 22:50:57 +0430

    Add my first MRI

---
+++ content/mri/view.php
@@ -0,0 +1,18 @@
+<?php
+namespace content\mri;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_("My first MRI"));
+		// \dash\data::page_desc(T_("Check or generate national code"));
+
+
+	}
+
+
+
+}
+?>
